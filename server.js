8dcda80c46abe2c52d4fbe9f5a6c35274cd107f5
Jesse-Tingle <jtingle0@gmail.com>
2020-02-18 22:23:25 -0500

    set up helmet

--- server.js
+++ server.js
@@ -1,11 +1,21 @@
 // libraries
 const express = require("express");
+const helmet = require("helmet");
+
+// file imports
+const userRoutes = require("./users/userRouter");
+const postRoutes = require("./posts/postRouter");
 
 // global objects
 const server = express();
 
 // middleware
+// server.use(logger)
+server.use(helmet());
 server.use(express.json());
+
+server.use("/api/user", userRoutes);
+server.use("/api/post", postRoutes);
 
 server.get("/", (req, res) => {
 	res.send(`<h2>Let's write some middleware!</h2>`);
